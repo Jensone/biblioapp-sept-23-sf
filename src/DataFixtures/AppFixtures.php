4334dde875947cb45f985d04ad3684249f59edf1
Jensone <hello@jensone.com>
2023-09-25 16:02:51 +0200

    ✨ Ajout de l'entité Category ✨ Execution des Fixteures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use Faker\Factory;
 use App\Entity\Book;
+use App\Entity\Category;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -13,6 +14,29 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create($fakerLocale = 'fr_FR');
         
+        $categories = [
+            'Roman',
+            'Bande dessiné',
+            'Poésie',
+            'Fantastique',
+            'Documentaire',
+            'Policier',
+            'Aventure',
+            'Biographie',
+        ];
+
+        $objectCategory = [];
+
+        for ($i=0; $i < count($categories); $i++) { 
+            $category = new Category;
+            $category->setName($categories[$i])
+                ->setDescription($faker->sentence(1))
+                ->setImage($faker->image())
+                ;
+            array_push($objectCategory, $category);
+            $manager->persist($category);
+        }        
+
         for ($i=0; $i < 500; $i++) { 
             $book = new Book();
             $book->setTitle($faker->sentence(1))
@@ -21,9 +45,13 @@ class AppFixtures extends Fixture
                 ->setPages(203)
                 ->setYear($faker->numberBetween(1950, 2023))
                 ->setIsAvailable($faker->boolean())
+                ->setCategory($objectCategory[rand(0, 7)])
                 ;
             $manager->persist($book);
         }
+
+
+
         $manager->flush();
     }
 }
