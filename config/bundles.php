4e43f5431bf7fe2b8b14a8e9a09c8920cfb8f3c7
Jensone <hello@jensone.com>
2023-10-11 15:43:56 +0200

    🔧 corriger(composer.json) : mettre à jour la version de symfony/maker-bundle à "^1.51" 🔧 corriger(config/bundles.php) : déplacer Symfony\Bundle\MakerBundle\MakerBundle de la section 'dev' à la section 'all' 🗑️ supprimer(Version20230925144455.php) : supprimer le fichier de migration Version20230925144455.php

--- config/bundles.php
+++ config/bundles.php
@@ -10,7 +10,7 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
